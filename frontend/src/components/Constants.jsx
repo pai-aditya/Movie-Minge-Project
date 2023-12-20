0d5e6e4f2e19948129ca4ea57d3d8c79c1147feb
pai-aditya <pai.aditya2011@gmail.com>
2023-12-20 12:47:08 +0530

    server url change

--- frontend/src/components/Constants.jsx
+++ frontend/src/components/Constants.jsx
@@ -1,5 +1,5 @@
 // export const SERVER_URL = "https://movieversebackend-yf41.onrender.com";
-export const SERVER_URL = "http://localhost:5555";
+export const SERVER_URL = "https://movie-verse-server-26n5eum7m-pai-aditya.vercel.app";
 // export const SERVER_URL = "https://themovieverseserver.netlify.app/.netlify/functions/api";
 
 // export const SERVER_URL = "https://themovieverse.cyclic.app";
